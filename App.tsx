4f80b3e239975c4a28d49db10e262fae1f1e0212
Borja Sanz de Bremond O'Neill <borjasbon@gmail.com>
2026-07-15 21:34:34 +0200

    Add web and svg support

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import BalanceResume from './src/components/balance-resume'
+import BalanceResume from './src/components/balance-resume';
+import Header from './src/components/header';
 
 export default function App() {
 	return (
 		<View style={ styles.container }>
+			<Header />
 			<BalanceResume />
 
 			<Text style={ styles.welcome }>Welcome Borchax!</Text>
@@ -19,8 +21,8 @@ const styles = StyleSheet.create({
 		backgroundColor: '#FFFFFF',
 		alignItems: 'center',
 		justifyContent: 'center',
-		paddingLeft: 25,
-		paddingRight: 25,
+		marginLeft: 25,
+		marginRight: 25,
 	},
 
 	welcome: {
---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Image } from 'react-native';
+import { ThemedText } from './ui/themed-text';
+
+import Logo from '../../assets/svg/return.svg';
+
+export default function Header() {
+    return (
+        <View style={ styles.header }>
+            <Logo style={ styles.return } />
+            <ThemedText style={ styles.title }>Current page</ThemedText>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        position: 'absolute',
+        top: 50,
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+    },
+
+    title: {
+        fontWeight: 700,
+        fontSize: 20,
+        marginStart: 10
+    },
+
+    return: {
+        width: 25,
+        height: 25,
+        marginTop: 3
+    }
+});
